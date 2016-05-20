44993c37ab128ee08a3e00644b5f8b0c2b9eadea
Peter Sbarski <peter.sbarski@gmail.com>
2016-05-20 10:40:23 +1000

    change to config

--- lesson-5/lambda/push-transcoded-url-to-firebase/config.js
+++ lesson-5/lambda/push-transcoded-url-to-firebase/config.js
@@ -2,5 +2,5 @@ var env={};
 env.BUCKET_REGION = 'us-east-1';
 env.FIREBASE_URL = 'YOUR FIREBASE URL HERE';
 env.FIREBASE_SECRET = 'YOUR FIREBASE SECRET HERE';
-env.S3 = 'https://s3.amazonaws.com/peter-transcoded-bucket';
+env.S3 = 'https://s3.amazonaws.com/YOUR_S3_TRANSCODE_BUCKET';
 module.exports = env;
