8711dd214d511e8cc44cf34353e1f3a7201a035b
Peter Sbarski <peter.sbarski@gmail.com>
2016-11-02 14:36:48 -0400

    Update index.js

--- lesson-5/lambda/push-transcoded-url-to-firebase/index.js
+++ lesson-5/lambda/push-transcoded-url-to-firebase/index.js
@@ -1,11 +1,22 @@
 'use strict';
+
 var AWS = require('aws-sdk');
 var firebase = require('firebase');
 var config = require('./config');
 
-exports.handler = function(event, context){
+// save the URL to firebase
+firebase.initializeApp({
+  serviceAccount: config.SERVICE_ACCOUNT,
+  databaseURL: config.DATABASE_URL
+});
+
+exports.handler = function(event, context, callback){
+    context.callbackWaitsForEmptyEventLoop = false;
+
     var key = event.Records[0].s3.object.key;
     var bucket = event.Records[0].s3.bucket.name;
+
+    var regionIdentifier = config.BUCKET_REGION === 'us-east-1' ? 's3' : 's3-' + config.BUCKET_REGION;
 
     var videoUrl = config.S3 + '/' + key;
 
@@ -16,21 +27,17 @@ exports.handler = function(event, context){
     // get the unique video key (the folder name)
     var uniqueVideoKey = sourceKey.split('/')[0];
 
-    // save the URL to firebase
-    firebase.initializeApp({
-      serviceAccount: config.SERVICE_ACCOUNT,
-      databaseURL: config.DATABASE_URL
-    });
-
     var database = firebase.database().ref();
 
     // update the unique entry for this video in firebase
     database.child('videos').child(uniqueVideoKey).set({
         transcoding: false,
         source: videoUrl
-    }).catch(function(err) {
-        console.log("Error writing transcoded video location to firebase");
-        console.log(err);
+    })
+    .then(function(){
+        callback(null, 'Added URL' + videoUrl);
+    })
+    .catch(function(err) {
         callback(err);
     });
 };
