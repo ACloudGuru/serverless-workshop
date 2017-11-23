f5dbc2c87fbb4adfa740d8a9663a5aeda1b6d04e
uchimanajet7 <uchimanajet7@gmail.com>
2017-11-23 20:43:18 +0900

    add new lambda function

---
+++ lab-6/lambda/delete-uploaded-video/index.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Manabu Uchida
+ * Updated by Manabu Uchida
+ * Last Updated: 11/23/2017
+ *
+ * Required Env Vars:
+ * UPLOAD_BUCKET
+ */
+
+'use strict';
+
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+exports.handler = function (event, context, callback) {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    var key = event.Records[0].Sns.Message;
+    var bucket = process.env.UPLOAD_BUCKET;
+    console.log("key: " + key);
+    console.log("bucket: " + bucket);
+
+    // Get object list from bucket and delete all.
+    // to-do: It is necessary to consider cases exceeding 1000 objects.
+    s3.listObjects({
+        Bucket: bucket,
+        Prefix: key,
+    }, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            callback(err);
+        } else {
+            var len = data.Contents.length;
+
+            for (var i = 0; i < len; i++) {
+                var obj = data.Contents[i];
+                s3.deleteObject({
+                    Bucket: bucket,
+                    Key: obj.Key
+                }, function (err, data) {
+                    if (err) {
+                        console.log(err);
+                        callback(err);
+                    }
+                });
+            }
+        }
+    });
+};
